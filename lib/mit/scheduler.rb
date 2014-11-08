808e347f522a8b0c20749bf74eb9074ceacd8591
Naoto Kaneko <naoty.k@gmail.com>
2014-11-08 12:58:21 +0900

    Tweet on schedules determined once per day

--- lib/mit/scheduler.rb
+++ lib/mit/scheduler.rb
@@ -1,21 +1,77 @@
 module MiT
   class Scheduler
+    SCHEDULE_UPDATE_INTERVAL = 24 * 60 * 60
     SCHEDULE_INTERVAL = 1 * 60
+    TWEET_COUNT_RANGE_PER_DAY = (10..20)
 
     def initialize
+      @threads = []
+      @scheduled_minutes = []
     end
 
     def start(&block)
-      loop do
-        block.call if on_time?
-        sleep(SCHEDULE_INTERVAL)
-      end
+      start_schedule_update
+      start_schedule(&block)
+      @threads.each(&:join)
     end
 
     private
 
+    def start_schedule_update
+      @threads << Thread.new do
+        loop do
+          determine_schedule!
+          sleep(SCHEDULE_UPDATE_INTERVAL)
+        end
+      end
+    end
+
+    def start_schedule(&block)
+      @threads << Thread.new do
+        loop do
+          block.call if on_time?
+          sleep(SCHEDULE_INTERVAL)
+        end
+      end
+    end
+
+    def determine_schedule!
+      @scheduled_minutes.clear
+
+      tweet_count_per_day = rand(TWEET_COUNT_RANGE_PER_DAY)
+      accumulated_probabilities = calculate_accumulated_probabilities
+      tweet_count_per_day.times do
+        r = rand
+        minute = accumulated_probabilities.index { |p| p > r }
+        @scheduled_minutes << minute
+        accumulated_probabilities.delete_at(minute)
+      end
+    end
+
+    def calculate_accumulated_probabilities
+      accumulated_probabilities = []
+      accumulated_probability = 0.0
+
+      tweets = Tweet.all
+      total_count = tweets.count
+      indexed_tweets = tweets.group_by(&:minute)
+
+      # minutes per day
+      1439.times do |minute|
+        tweets = indexed_tweets[minute]
+        tweet_count = tweets.nil? ? 0 : tweets.count
+        probability = tweet_count.to_f / total_count
+        accumulated_probability += probability
+        accumulated_probabilities << accumulated_probability
+      end
+
+      accumulated_probabilities
+    end
+
     def on_time?
-      true
+      now = Time.now
+      minute = now.hour * 60 + now.min
+      @scheduled_minutes.include?(minute)
     end
   end
 end
