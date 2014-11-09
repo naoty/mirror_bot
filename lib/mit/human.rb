93ea13a021d4e231f9cf86cdbb1a4720c719e27f
Naoto Kaneko <naoty.k@gmail.com>
2014-11-09 13:52:05 +0900

    Fix a bug that causes to save all tweets on human's timeline

--- lib/mit/human.rb
+++ lib/mit/human.rb
@@ -9,6 +9,13 @@ module MiT
         config.access_token = ENV["HUMAN_ACCESS_TOKEN"]
         config.access_token_secret = ENV["HUMAN_ACCESS_TOKEN_SECRET"]
       end
+      rest_client = Twitter::REST::Client.new do |config|
+        config.consumer_key = ENV["HUMAN_CONSUMER_KEY"]
+        config.consumer_secret = ENV["HUMAN_CONSUMER_SECRET"]
+        config.access_token = ENV["HUMAN_ACCESS_TOKEN"]
+        config.access_token_secret = ENV["HUMAN_ACCESS_TOKEN_SECRET"]
+      end
+      @user = rest_client.user(skip_status: true)
       @trainer = Trainer.new
     end
 
@@ -16,7 +23,7 @@ module MiT
       @streaming_client.user do |object|
         case object
         when Twitter::Tweet
-          @trainer.train_tweet(object)
+          @trainer.train_tweet(object) if object.user.id == @user.id
           # TODO: Train non favorites
         when Twitter::Streaming::Event
           # TODO: Train favorites
